5bc7b4f5f9e90d9510ebb7b11c91572b37522741
Aayushi <aaayushi971@gmail.com>
2025-04-23 21:30:05 -0700

    error fixes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Menu, X, Moon, Sun } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 // import { useTheme } from '../theme/ThemeProvider';
 
 const navLinks = [
