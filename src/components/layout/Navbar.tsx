d10bd19e34d231b6601c1a6ab7c1f3de6f819c9b
Aayushi <aaayushi971@gmail.com>
2025-04-23 21:33:20 -0700

    error fixes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 // src/components/Navigation.tsx
 'use client';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Menu, X } from 'lucide-react';
